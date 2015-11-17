b6038caffd09607c7957c6133ea18bab116f3415
Will Jones <willgreat5@hotmail.com>
2015-11-17 09:45:21 -0800

    Began work on cpuid

---
+++ kernel/arch/i386/cpuid.c
@@ -0,0 +1,14 @@
+/* Vendor-strings. */
+#define CPUID_VENDOR_OLDAMD       "AMDisbetter!" //old AMD
+#define CPUID_VENDOR_AMD          "AuthenticAMD"
+#define CPUID_VENDOR_INTEL        "GenuineIntel"
+#define CPUID_VENDOR_VIA          "CentaurHauls"
+#define CPUID_VENDOR_OLDTRANSMETA "TransmetaCPU"
+#define CPUID_VENDOR_TRANSMETA    "GenuineTMx86"
+#define CPUID_VENDOR_CYRIX        "CyrixInstead"
+#define CPUID_VENDOR_CENTAUR      "CentaurHauls"
+#define CPUID_VENDOR_NEXGEN       "NexGenDriven"
+#define CPUID_VENDOR_UMC          "UMC UMC UMC "
+#define CPUID_VENDOR_SIS          "SiS SiS SiS "
+#define CPUID_VENDOR_NSC          "Geode by NSC"
+#define CPUID_VENDOR_RISE         "RiseRiseRise"
